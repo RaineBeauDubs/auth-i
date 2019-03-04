739a005ca5b92cc1799ca511a77dae052cd29fe7
Raine Wallace <rainebeaudubs@gmail.com>
2019-03-04 16:04:22 -0500

    Added users table migration, added helper, am not able to register user.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const bcrypt = require('bcryptjs');
 
 const db = require('./data/dbConfig');
+const Users = require('./helpers/users-module');
 
 
 const server = express();
@@ -16,6 +17,26 @@ server.use(cors());
 
 server.get('/', (req, res) => {
   res.send("Welcome to Auth-I!")
+});
+
+// REGISTER
+
+server.post('/api/register', (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 16)
+  user.password = hash
+
+  Users.add(user)
+    .then(saved => {
+      res
+        .status(201)
+        .json(saved);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json(error);
+    });
 });
 
 const port = 5000;
